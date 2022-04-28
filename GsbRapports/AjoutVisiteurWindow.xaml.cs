9a10a90dd082eb8836bb4e8949b06e2884113d5d
Salim <tizi_salim@yahoo.fr>
2022-04-28 09:24:34 +0200

    new project gsbRapports

---
+++ GsbRapports/AjoutVisiteurWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using dllRapportVisites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+
+namespace GsbRapports
+{
+    /// <summary>
+    /// Logique d'interaction pour AjoutVisiteurWindow.xaml
+    /// </summary>
+    public partial class AjoutVisiteurWindow : Window
+    {
+        private Secretaire laSecretaire;
+        private WebClient wb;
+        private string site;
+
+        public AjoutVisiteurWindow(Secretaire laSecretaire, WebClient wb, string site)
+        {
+            InitializeComponent();
+            this.laSecretaire = laSecretaire;
+            this.wb = wb;
+            this.site = site;
+        }
+
+        private void btnValider_Click1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                string id = this.txtIdVisiteur.Text;
+                string cp = this.txtCodePostal.Text;
+                string nom = this.txtNom.Text;
+                string prenom = this.txtPrenom.Text;
+                string adresse = this.txtAdresse.Text;
+                string ville = this.txtVille.Text;
+
+                bool ok = true;
+
+                if (id.Length != 3)
+                {
+                    ok = false;
+                    erreurId.Text = "L'id doit contenir 3 caracteres";
+                }
+                else
+                {
+                    erreurId.Text = "";
+                }
+
+                if (nom.Length == 0)
+                {
+                    ok = false;
+                    erreurNom.Text = "Veuillez entrer un nom";
+                }
+                else
+                {
+                    erreurNom.Text = "";
+                }
+
+                if (prenom.Length == 0)
+                {
+                    ok = false;
+                    erreurPrenom.Text = "Veuillez entrer un prénom";
+                }
+                else
+                {
+                    erreurPrenom.Text = "";
+                }
+
+                if (adresse.Length == 0)
+                {
+                    ok = false;
+                    erreurAdresse.Text = "Veuillez entrer une adresse";
+                }
+                else
+                {
+                    erreurAdresse.Text = "";
+                }
+
+                if (cp.Length != 5)
+                {
+                    ok = false;
+                    erreurCp.Text = "Le code postale doit contenir 5 caractères";
+                }
+                else
+                {
+                    erreurCp.Text = "";
+                }
+
+
+                if (ville.Length == 0)
+                {
+                    ok = false;
+                    erreurVille.Text = "Veuillez entrer une ville";
+                }
+                else
+                {
+                    erreurVille.Text = "";
+                }
+
+
+                if (ok)
+                {
+                    string url = this.site + "visiteurs";
+                    NameValueCollection parametres = new NameValueCollection();
+                    parametres.Add("ticket", this.laSecretaire.getHashTicketMdp());
+                    parametres.Add("idVisiteur", id);
+                    parametres.Add("nom", nom);
+                    parametres.Add("prenom", prenom);
+                    parametres.Add("adresse", adresse);
+                    parametres.Add("cp", cp);
+                    parametres.Add("ville", ville);
+                    string date1 = this.txtDateEmbauche.Text;
+                    DateTime dTime = Convert.ToDateTime(date1);
+                    string dateTime1 = dTime.ToString("yyyy-MM-dd");
+                    parametres.Add("dateEmbauche", dateTime1);
+                    byte[] tabByte = wb.UploadValues(url, parametres); // envoie des donnés en post 
+                    string ticket = UnicodeEncoding.UTF8.GetString(tabByte);
+                    this.laSecretaire.ticket = ticket.Substring(2);// anti slash n 
+                    MessageBox.Show("Ajout d'un visiteur effectué");
+                }
+
+
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response is HttpWebResponse)
+                    MessageBox.Show(((HttpWebResponse)ex.Response).StatusCode.ToString());
+            }
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ GsbRapports/ajoutFamilleWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net;
+using dllRapportVisites;
+using Newtonsoft.Json;
+using System.Collections.Specialized;//pour api
+
+namespace GsbRapports
+{
+    /// <summary>
+    /// Logique d'interaction pour ajoutFamilleWindow.xaml
+    /// </summary>
+    public partial class ajoutFamilleWindow : Window
+    {
+        private Secretaire laSecretaire;
+        private WebClient wb;
+        private string site;
+
+        public ajoutFamilleWindow(Secretaire laSecretaire, WebClient wb, string site)
+        {
+            InitializeComponent();
+            this.laSecretaire = laSecretaire;
+            this.site = site;
+            this.wb = wb;
+        }
+
+        private void btnValider_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string url = this.site + "familles";
+                NameValueCollection n = new NameValueCollection();
+                n.Add("ticket", this.laSecretaire.getHashTicketMdp());
+                n.Add("idFamille", txtId.Text.ToString());
+                n.Add("libelle", txtLibelle.Text);
+                byte[] tabByte = wb.UploadValues(url, n);
+                string ticket = UnicodeEncoding.UTF8.GetString(tabByte);
+                this.laSecretaire.ticket = ticket.Substring(2);//methode qui enleve les 2 derniers caracteres
+
+                MessageBox.Show("Vous avez bien ajouté la famille");
+
+
+            }
+            catch (WebException exc)
+            {
+                if (exc.Response is HttpWebResponse)
+                    MessageBox.Show(((HttpWebResponse)exc.Response).StatusCode.ToString());
+            }
+        }
+    }
+}
+
